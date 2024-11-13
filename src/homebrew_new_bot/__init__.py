abbe118404714e49ee018b6dc166d24a3085c60a
Vince Cima <contact@vincecima.com>
2024-11-13 11:11:37 -0600

    Switch to storing compressed API output

--- src/homebrew_new_bot/__init__.py
+++ src/homebrew_new_bot/__init__.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import gzip
 import json
-||||||| parent of 391e729 (Start tracking and posting for formula)
-import email.utils
-=======
-import json
->>>>>>> 391e729 (Start tracking and posting for formula)
 import logging
 from datetime import datetime, timezone
 from enum import StrEnum
